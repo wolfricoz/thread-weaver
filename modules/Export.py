fdb8e28ad376f6cc852d621b07953f9d2f9ac444
ricoi <ricoisdenaam@gmail.com>
2026-02-18 21:51:17 +0100

    Add forum export functionality to Export.py

--- modules/Export.py
+++ modules/Export.py
@@ -3,7 +3,7 @@ import logging
 import discord
 from discord import app_commands
 from discord.ext.commands import Bot, GroupCog
-from discord_py_utilities.messages import send_message, send_response
+from discord_py_utilities.messages import send_message
 
 from classes.kernel.AccessControl import AccessControl
 from classes.support.ThreadArchive import ThreadArchive
@@ -26,32 +26,71 @@ class Export(GroupCog, name="export") :
 		- `Manage Threads`
 		- `Premium Access`
 		"""
-		buttons = ConfirmButtons(confirm_message=f"Creating an export of `{thread.name}`, this may take a while... The results will be sent to your DMs")
+		buttons = ConfirmButtons(
+			confirm_message=f"Creating an export of `{thread.name}`, this may take a while... The results will be sent to your DMs")
 		result = await buttons.send_confirmation(interaction,
-		                                 f"Are you sure you want to export `{thread.name}`? This will create a .zip file containing the thread's messages and image attachments. if you delete the thread after export, non-image attachments may not work as they rely on the url provided by discord, which is deleted when the message/thread is deleted. Threads with a large amount of images may fail to export due to the size of the .zip file. If you have any issues with the export, please contact support.",
-		                                 )
-		if not result:
+		                                         f"Are you sure you want to export `{thread.name}`? This will create a .zip file containing the thread's messages and image attachments. if you delete the thread after export, non-image attachments may not work as they rely on the url provided by discord, which is deleted when the message/thread is deleted. Threads with a large amount of images may fail to export due to the size of the .zip file. If you have any issues with the export, please contact support.",
+		                                         )
+		if not result :
 			logging.info(f"{interaction.user} cancelled the export of {thread.name}")
 			return
 		export_class = ThreadArchive(interaction.guild.name + "_" + thread.name, channel=thread)
 		await export_class.run()
-		try:
-			await send_message(interaction.user, f"Here is your export of `{thread.name}`:", files=[discord.File(export_class.zip_path)])
-		except discord.Forbidden:
-			await send_message(interaction.channel, f"{interaction.user.mention}, I was unable to send you the export in DMs. Please check your DM settings and try again.")
-		except Exception as e:
-			await send_message(interaction.user, f"{interaction.user.mention}, an error occurred while sending you the export: {e}")
+		try :
+			await send_message(interaction.user, f"Here is your export of `{thread.name}`:",
+			                   files=[discord.File(export_class.zip_path)])
+		except discord.Forbidden :
+			await send_message(interaction.channel,
+			                   f"{interaction.user.mention}, I was unable to send you the export in DMs. Please check your DM settings and try again.")
+		except Exception as e :
+			await send_message(interaction.user,
+			                   f"{interaction.user.mention}, an error occurred while sending you the export: {e}")
 			logging.error(e, exc_info=True)
 			return
 		await export_class.clean_up()
-		if not delete:
+		if not delete :
 			return
-		try:
+		try :
 			await thread.delete()
-		except Exception as e:
-			await send_message(interaction.channel, f"{interaction.user.mention}, an error occurred while deleting the thread: {e}", error_mode="ignore")
+		except Exception as e :
+			await send_message(interaction.channel,
+			                   f"{interaction.user.mention}, an error occurred while deleting the thread: {e}",
+			                   error_mode="ignore")
 			logging.error(e, exc_info=True)
 
+	@app_commands.command(name="forum", description="Creates an export of an entire forum")
+	@app_commands.checks.has_permissions(manage_threads=True)
+	@AccessControl().check_premium()
+	async def forum(self, interaction: discord.Interaction, forum: discord.ForumChannel) :
+		"""
+		Creates an export of a specific thread. This will create a .zip file containing the thread's messages and attachments. The file will be sent to the user who invoked the command.
+
+		**Permissions:**
+		- `Manage Threads`
+		- `Premium Access`
+		"""
+		buttons = ConfirmButtons(
+			confirm_message=f"Creating an export of `{forum.name}`, this may take a while... The results will be sent to your DMs")
+		result = await buttons.send_confirmation(interaction,
+		                                         f"Are you sure you want to export `{forum.name}`? This will create a .zip file containing the thread's messages and image attachments. if you delete the thread after export, non-image attachments may not work as they rely on the url provided by discord, which is deleted when the message/thread is deleted. Threads with a large amount of images may fail to export due to the size of the .zip file. If you have any issues with the export, please contact support.\n\n**note** Forums have a high chance of failing to export due to the large amount of messages and attachments they can contain. It is recommended to export individual threads within the forum instead of the entire forum to ensure a successful export.",
+		                                         )
+		if not result :
+			logging.info(f"{interaction.user} cancelled the export of {forum.name}")
+			return
+		export_class = ThreadArchive(interaction.guild.name + "_" + forum.name, channel=forum)
+		await export_class.run()
+		try :
+			await send_message(interaction.user, f"Here is your export of `{forum.name}`:",
+			                   files=[discord.File(export_class.zip_path)])
+		except discord.Forbidden :
+			await send_message(interaction.channel,
+			                   f"{interaction.user.mention}, I was unable to send you the export in DMs. Please check your DM settings and try again.")
+		except Exception as e :
+			await send_message(interaction.user,
+			                   f"{interaction.user.mention}, an error occurred while sending you the export: {e}")
+			logging.error(e, exc_info=True)
+			return
+		await export_class.clean_up()
 
 
 async def setup(bot: Bot) :
