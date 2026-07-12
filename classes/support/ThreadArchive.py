96a3062361c19ba109dd4f4ce75a826e3af1e801
ricoi <ricoisdenaam@gmail.com>
2026-07-12 02:30:00 +0200

    enabled MB check.

--- classes/support/ThreadArchive.py
+++ classes/support/ThreadArchive.py
@@ -410,9 +410,9 @@ class ThreadArchive():
 
 		mb = os.stat(self.zip_path).st_size / (1024 * 1024)
 		logging.info(f"File is: {mb} MB")
-		# if mb < 24:
-		# 	logging.info("under 24 MB")
-		# 	return None
+		if mb < 24:
+			logging.info("under 24 MB")
+			return None
 
 		password = ''.join(secrets.choice(string.ascii_uppercase + string.digits) for _ in range(8))
 		with open(self.zip_path, 'rb') as f:
